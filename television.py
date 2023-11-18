36bf674e3c5b7ec08f4bda22859baf001c6cf3a8
djseth-81 <sethdjohnson81@gmail.com>
2023-11-17 18:48:25 -0600

    Updating branch with test module and modified class object

--- television.py
+++ television.py
@@ -2,40 +2,86 @@
 :Author: Seth Johnson
 :Date: 11-13-2023
 :Description:
-
+This script houses the object Television to represent its basic behavior. It is called in main.py for operation.
 """
-### Import packages ###
 
 ### Class definition ###
 class Television:
     """
-    TODO:
-    - How to manage volume when muted
-        - do I save the volume? does it get reset to MIN?
+    A class representing a Television.
+
+    Attributes
+    ----------
+    status : bool
+        Determines if the television (tv) is powered on or not
+    muted : bool
+        Determines if the tv has been muted, reverting the volume value to 0
+    volume : int
+        Represents the volume of the tv
+    channel : int
+        Represents the selected channel of the tv
+    
+    Methods
+    -------
+    power():
+        Toggles the status boolean to turn the tv on and off
+    mute():
+        Toggles the muted boolean to mute and unmute the tv. When the tv is muted, the volume is displayed as 0
+    channel_up():
+        Incraments the channel variable until the maximum channel value is reached, after which cycles back to the minimum channel value.
+    channel_down():
+        Decraments the channel variable until the minimum channel value is reached, after which it cycles to the maximum channel value
+    volume_up():
+        Incraments the volume variable until the maximum volume value is reached.
+    volume_down():
+        Decrements the volume variable until the minimum volume vaule is reached.
+    powered():
+        Returns the state of the status boolean
+    muted():
+        Returns the state of the muted boolean
+    getVolume():
+        Returns the integer value of volume
+    getChannel():
+        Returns the integer value of channel
+    __str__():
+        returns a formatted string which calls powered(), getChannel(), and getVolume() to display the given state of a tv object
     """
+
     ### Class Variables ###
-    MIN_VOLUME = 0
-    MAX_VOLUME = 2
-    MIN_CHANNEL = 0
-    MAX_CHANNEL = 3
+    MIN_VOLUME: int = 0
+    MAX_VOLUME: int = 2
+    MIN_CHANNEL: int= 0
+    MAX_CHANNEL: int= 3
 
     ### Constructors ###
-    def __init__(self):
-        self.__status = False
-        self.__muted = False
-        self.__volume = self.MIN_VOLUME
-        self.__channel = self.MIN_CHANNEL
+    def __init__(self) -> None:
+        """
+        Constructs the instance attributes for the tv object
+        """
+        self.__status: bool = False
+        self.__muted: bool = False
+        self.__volume: int = self.MIN_VOLUME
+        self.__channel: int = self.MIN_CHANNEL
 
     ### Mutators ###
-    def power(self):
+    def power(self) -> None:
+        """
+        Toggles the status boolean to turn the tv on and off
+        """
         self.__status = False if self.powered() else True
     
-    def mute(self):
+    def mute(self) -> None:
+        """
+        Toggles the mute boolean to mute and unmute the tv.
+        """
         # Mutes and unmutes TV
         if self.powered():
             self.__muted = False if self.muted() else True
 
-    def channel_up(self):
+    def channel_up(self) -> None:
+        """
+        Incraments the channel variable until the maximum channel value is reached, after which cycles back to the minimum channel value.
+        """
         # Increments tv channel value when tv is on
         if self.powered():
             if self.__channel < self.MAX_CHANNEL:
@@ -44,7 +90,10 @@ class Television:
                 # If on maximum channel value and called again, cycles back to min channel value
                 self.__channel = self.MIN_CHANNEL
     
-    def channel_down(self):
+    def channel_down(self) -> None:
+        """
+        Decraments the channel variable until the minimum channel value is reached, after which it cycles to the maximum channel value
+        """
         # Decrements tv channel value when tv is on.
         if self.powered():
             if self.__channel > self.MIN_CHANNEL:
@@ -53,7 +102,10 @@ class Television:
                 # Cycles back to max channel called at min value
                 self.__channel = self.MAX_CHANNEL
     
-    def volume_up(self):
+    def volume_up(self) -> None:
+        """
+        Incraments the volume variable until the maximum volume value is reached.
+        """
         # incraments tv volume when tv is on. remains at maximum when called at max value
         if self.powered():
             if self.muted():
@@ -62,7 +114,10 @@ class Television:
             if self.__volume < self.MAX_VOLUME:
                 self.__volume += 1
     
-    def volume_down(self):
+    def volume_down(self) -> None:
+        """
+        Decrements the volume variable until the minimum volume vaule is reached.
+        """
         # decrements tv volume when tv is on. remains at min value when called at min value
         if self.powered():
             if self.muted():
@@ -72,28 +127,37 @@ class Television:
                 self.__volume -= 1
 
     ### Accessors ###
-    def powered(self): # returns status boolean
+    def powered(self) -> bool:
+        """
+        Returns the state of the status boolean
+        :return: self.__status
+        """
         return self.__status
 
-    def muted(self): # returns muted boolean
+    def muted(self) -> bool:
+        """
+        Returns the state of the muted boolean
+        :return: self.__muted
+        """
         return self.__muted
 
-    def getVolume(self): # returns volume int value
+    def getVolume(self) -> int:
+        """
+        Returns the integer value of volume
+        :return: 0 if the tv is muted or self.__volume otherwise
+        """
         return 0 if self.muted() else self.__volume
         
-    def getChannel(self): # returns channel value
+    def getChannel(self) -> int:
+        """
+        Returns the integer value of channel
+        :return: self.__channel
+        """
         return self.__channel
         
-    def __str__(self):
-        return f"Power- {self.powered()}, Channel-{self.getChannel()}, Volume-{self.getVolume()}"
-    
-"""
-TODO:
-- create a new branch named test
-- add docstrings and typehinting to television.py
-- create test_television.py
-    - write unit tests for each of the methods using pytest
-- Commit changes to test branch
-- push changes to GitHub repository
-- get link of my public remote repository
-"""
+    def __str__(self) -> str:
+        """
+        Returns a formatted string which calls powered(), getChannel(), and getVolume() to display the given state of a tv object
+        :return: string of the power status, channel value, and volume value
+        """
+        return f"Power - {self.powered()}, Channel - {self.getChannel()}, Volume - {self.getVolume()}"
